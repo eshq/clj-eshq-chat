2c784499e9bbe2783efd72a7f06198115f6a8a1a
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2012-09-08 15:31:47 +0200

    Compojure working

--- src/chat/web.clj
+++ src/chat/web.clj
@@ -1,10 +1,16 @@
 (ns chat.web
   (:use [compojure.core])
-  (:require [ring.adapter.jetty :as ring]))
+  (:require [compojure.handler :as handler]
+            [ring.adapter.jetty :as ring]))
 
 (defroutes main-routes
            (GET "/" [] "<h2>Hello, World!</h2>"))
 
+(def application (handler/site main-routes))
+
+(defn start [port]
+  (ring/run-jetty #'application {:port (or port 8080) :join? false}))
+
 (defn -main []
-  (let [port (Integer/parseInt (System/getenv "PORT"))]
-    (ring.run-jetty #'main-routes {:port port})))
+  (let [port (Integer. (System/getenv "PORT"))]
+    (start port)))
